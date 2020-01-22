f0b4f3b5cf28ec3edfee547240778ad240ccfdbe
Katie Forthman <kclary@laureateinstitute.org>
2020-01-22 15:35:53 -0600

    This is a test

--- rGFA_simu_fcn.R
+++ rGFA_simu_fcn.R
@@ -1,4 +1,5 @@
 ##### For true W matrix parameters #####
+# This is a test commit
 W_heatmap <- function(W_DxK, varIdx.by.block, block.names){
   gr <- varIdx.by.block; M <- length(gr)
   names(gr) <- block.names
